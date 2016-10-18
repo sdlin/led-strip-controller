41b584fdd0f7beae5be6475e85303572ef608837
Spencer Lin <spencerlin@gatech.edu>
2016-10-17 21:17:38 -0700

    Add on off to api.

--- server.py
+++ server.py
@@ -20,8 +20,19 @@ class GpioPwm(object):
         self.r = 0
         self.g = 0
         self.b = 0
+        self.last_r = 255
+        self.last_g = 255
+        self.last_b = 255
+        self.state = 'off'
 
-    def set_rgb(self, r_value, g_value, b_value):
+    def set_rgb(self, r_value, g_value, b_value, reset=True):
+        if reset and r_value == 0 and g_value == 0 and b_value == 0:
+            self.last_r = 255
+            self.last_g = 255
+            self.last_b = 255
+            self.state = 'off'
+        elif self.state == 'off':
+            self.state = 'on'
         self.set_red(r_value)
         self.set_green(g_value)
         self.set_blue(b_value)
@@ -37,6 +48,24 @@ class GpioPwm(object):
     def set_blue(self, value):
         self._set_pin_value('b', value)
         self.b = value
+
+    def turn_off(self):
+        if self.state == 'off':
+            return 1
+        else:
+            self.last_r = self.r
+            self.last_g = self.g
+            self.last_b = self.b
+            self.set_rgb(0, 0, 0, False)
+            self.state = 'off'
+            return 0
+
+    def turn_on(self):
+        if self.state == 'on':
+            return 1
+        else:
+            self.set_rgb(self.last_r, self.last_g, self.last_b)
+            return 0
 
     def _set_pin_value(self, color, value):
         pin = GpioPwm.rgb_to_pin[color]
@@ -66,6 +95,36 @@ def rgb_handler():
         )
 
 
+@app.route('/api/simple_on', methods=['POST'])
+def simple_on_handler():
+    global gpiopwm
+    return simple_closure_handler(gpiopwm.turn_on)
+
+
+@app.route('/api/simple_off', methods=['POST'])
+def simple_off_handler():
+    global gpiopwm
+    return simple_closure_handler(gpiopwm.turn_off)
+
+
+def simple_closure_handler(func):
+    ret = func()
+    if ret == 0:
+        msg = 'SUCCESS'
+    else:
+        msg = 'FAILURE'
+
+    msg = 'STATUS: ' + msg
+    return Response(
+        json.dumps(msg),
+        mimetype='application/json',
+        headers={
+            'Cache-Control': 'no-cache',
+            'Access-Control-Allow-Origin': '*'
+        }
+    )
+
+
 def make_rgb_response(r=255, g=255, b=255):
     """Return response."""
     return Response(
@@ -80,4 +139,5 @@ def make_rgb_response(r=255, g=255, b=255):
 if __name__ == '__main__':
     global gpiopwn
     gpiopwm = GpioPwm()
+    gpiopwm.set_rgb(0, 0, 0)
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
